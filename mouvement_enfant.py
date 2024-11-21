d34bba36eb9286fe5056d51fc926a30fa7c9041d
Ejindo06 <emma.didion@student.uclouvain.be>
2024-11-21 18:09:03 +0100

    Correction + view milk + initialisation berceuse/alarme

--- mouvement_enfant.py
+++ mouvement_enfant.py
@@ -1,29 +1,73 @@
 from microbit import *
 import radio
+import music
 
 radio.on()
 key = "Key" 
 
+biberon = Image("19991:""09090:""92229:""09290:""09290")
+
+def berceuse():
+    music.play(music.PRELUDE)
+
+def alarme():
+    music.play(music.PYTHON)
+    
+
+def milk_quantity(milk):
+    for _ in range(5):
+        display.show(milk)
+        sleep(200)
+        display.show(biberon)
+        sleep(1000)
+        sleep
+
 def movement():
+    last_state = None
     while True:
-        
+        message = radio.receive()
+        if pin_logo.is_touched():
+            return False
+        if message == "berceuse":
+            berceuse()
+        elif message == "alarme":
+            alarme()
+            
         x = accelerometer.get_x()
         y = accelerometer.get_y()
         z = accelerometer.get_z()
-        movement_intensite = x**2 + y**2 + z**2
-        if movement_intensite < 20000:
-            state="Endormei"
-        elif 20000 <= movement_intensite < 100000:
+        movement_intensite = (x**2 + y**2 + z**2)*0.5
+        if movement_intensite < 1000000:
+            state="Endormi"
+        elif 1000000 <= movement_intensite < 2000000:
             state="Agité"
         else:  
             state="Trés_agité"
-        radio.send(state)
+        
         if state=="Endormi":
             display.show(Image.SMILE)
         elif state=="Agité":
             display.show(Image.SAD)
         elif state=="Trés_agité":
             display.show(Image.ANGRY)
-        sleep(1000)
+
+        if last_state != state:
+            radio.send(state)
+        last_state = state
+        sleep(100)
+
+display.show(Image.HEART_SMALL)
+sleep(700)
         
-movement()
+milk = 0
+while True:
+    message = radio.receive()
+    try:
+        milk = int(message)
+    except TypeError:
+        pass
+    
+    if pin_logo.is_touched():
+        milk_quantity(milk)            
+    else:
+        movement()
