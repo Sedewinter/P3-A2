5a5d8d57db22a49f5b478c1930d3d62f6929c59b
Triple T <git@triplet.slmail.me>
2024-11-26 17:37:57 +0100

    Ajout d'un détecteur de grimpage

--- mouvement_enfant.py
+++ mouvement_enfant.py
@@ -1,5 +1,6 @@
 from microbit import *
 import radio
+from math import *
 
 radio.on()
 radio.config(group=99)
@@ -7,11 +8,14 @@ key = "Key"
 
 def movement():
     while True:
-        
         x = accelerometer.get_x()
         y = accelerometer.get_y()
         z = accelerometer.get_z()
+        z_movement = abs(z - 1024)
         movement_intensite = x**2 + y**2 + z**2
+        grimpage_intensite = sqrt(x) + sqrt(y) + z**3
+        if z_movement>500000:
+            state=detection_grimpage()
         if movement_intensite < 20000:
             state="Endormi"
         elif 20000 <= movement_intensite < 100000:
@@ -27,3 +31,15 @@ def movement():
             display.show(Image.ANGRY)
         sleep(1000)    
 movement()
+
+def detection_grimpage():
+    x = accelerometer.get_x()
+    y = accelerometer.get_y()
+    z = accelerometer.get_z()
+    z_movement = abs(z - 1024)
+    climbing_duration=0
+    while z_movement>500000:
+        climbing_duration+=1
+        if climbing_duration>10:
+            state="CLIMBING"
+            return state
